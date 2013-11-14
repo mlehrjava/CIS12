e27e0e0c80d79660de054f3106488b3b54322d0d
angrymasteryoda <angrymasteryoda@gmail.com>
2013-11-14 12:58:56 -0800

    Signed-off-by: angrymasteryoda <angrymasteryoda@gmail.com>

--- mr2358174/survey/assets/inc/footer.php
+++
@@ -1,8 +0,0 @@
-<footer id="footer">
-    <p>
-        &copy; Copyright  by Michael Risher
-    </p>
-</footer>
-<?php
-Core::loadJavascript();
-?>
--- mr2358174/survey/templates/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-include '../assets/inc/meta.php';
-echo '<textarea>';
-echo Security::sanitize('<lksd>alsk" \'hdlskahdlsahdioejd lasjd lsaj</lksd>clean
-
-');
-echo '<lksd>alskdjaslkdhlaskhdlskahdlsahdioejd lasjd lsaj</lksd>clean';
-echo '</textarea>';
